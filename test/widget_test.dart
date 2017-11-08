5dbf191f4b70ec95f17f0c8655a61542efec63ed
Daniel Sauvé <djsauve@rogers.com>
2017-11-08 09:45:56 -0500

    Updated test to work with current changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,7 +15,7 @@ void main() {
     await tester.pumpWidget(new MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
@@ -23,7 +23,8 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsNothing);
   });
 }
